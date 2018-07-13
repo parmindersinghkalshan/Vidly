9a177067190a4677bbf23546cfd7abaa860b9b2f
Parminder Singh Kalshan <parmindersinghkalshan@gmail.com>
2018-07-12 21:24:57 -0400

    Revert "Removed unused usings"
    This reverts commit 2d25f5c2aca316208ab447dcc9989c9f2da4afc2.

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -1,4 +1,8 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
 using Vidly.Dtos;
 using Vidly.Models;
 
--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
 using Vidly.Models;
 
 namespace Vidly.Dtos
